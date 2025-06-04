72d4cc99abc313b989c8422323a460fd58ad1336
IffaHesti <iffahesti374@gmail.com>
2025-06-04 13:22:55 +0700

    Update test_stock.py

--- test-driven-dev/tests/test_stock.py
+++ test-driven-dev/tests/test_stock.py
@@ -1,5 +1,7 @@
+import unittest
+from unittest.mock import patch, MagicMock
 import pytest
-from BouquetShop import update_stock, get_stock_level
+from stock_logic import update_stock, get_stock_level
 
 def test_update_stock():
     bunga_id = 1  # ID bunga contoh
