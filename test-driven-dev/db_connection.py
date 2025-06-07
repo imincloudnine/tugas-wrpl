571f2dc156909f359f438278c4cc747d1234441a
IffaHesti <iffahesti374@gmail.com>
2025-06-07 17:46:25 +0700

    Update db_connection.py

--- test-driven-dev/db_connection.py
+++ test-driven-dev/db_connection.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="bouquetshop"
+        host=os.environ.get('DB_HOST'),
+        port=int(os.environ.get('DB_PORT', 3306)),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        database=os.environ.get('DB_NAME')
     )
